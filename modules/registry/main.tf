b32a89bcef927aa0ece61a09364de473afd61f74
raphaeljoie <9681129+raphaeljoie@users.noreply.github.com>
2026-07-07 22:40:01 +0200

    storage account name from id

--- modules/registry/main.tf
+++ modules/registry/main.tf
@@ -3,14 +3,15 @@ locals {
   requires_container_id = length(var.artifacts) > 0
 
   storage_account_create = var.storage_account_create && var.storage_account_id == null && var.container_id == null
-  storage_account_load = local.requires_storage_account_id && !local.storage_account_create
+  storage_account_load = local.requires_storage_account_id && var.storage_account_id == null && !local.storage_account_create
   storage_account_id = (var.storage_account_id != null ? var.storage_account_id :
       local.storage_account_create ? azurerm_storage_account.artifacts[0].id :
         local.storage_account_load ? data.azurerm_storage_account.artifacts[0].id :
         null)
   storage_account_name = (var.storage_account_name != null ? var.storage_account_name :
       local.storage_account_create ? azurerm_storage_account.artifacts[0].name :
-        local.storage_account_load ? data.azurerm_storage_account.artifacts[0].name : null)
+        local.storage_account_load ? data.azurerm_storage_account.artifacts[0].name :
+          local.storage_account_id != null ? reverse(split("/", local.storage_account_id))[0] : null)
 
   container_create = var.container_create && var.container_id == null
   container_load = !var.container_create && var.container_id == null && local.requires_container_id
